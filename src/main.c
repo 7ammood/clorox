2f50c97639090c9ede91fd4d10c241018ac86ece
7amood <mohammadsulaiman206@gmail.com>
2026-05-22 20:49:52 +0300

    finished the parser !!!!

--- src/main.c
+++ src/main.c
@@ -31,7 +31,7 @@ static char* readFile(const char* path){
 
     fseek(file,0L,SEEK_END);
     size_t fileSize = ftell(file);
-    rewind(file);
+    rewind(file);   
 
     
     char* buffer = (char*)malloc(fileSize + 1);
