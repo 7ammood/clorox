0a9c7122cad0ee6446aadd023196fffbb395236c
7amood <mohammadsulaiman206@gmail.com>
2026-05-28 15:56:25 +0300

    added null and boolean values
    still working on strings wish me luck <3

--- src/value.c
+++ src/value.c
@@ -28,5 +28,15 @@ void freeValueArray(ValueArray* array){
 }
 
 void printValue(Value value){
-    printf("%g", value);
+    printf("%g", AS_NUMBER(value));
+}
+
+bool valuesEqual(Value a, Value b) {
+  if (a.type != b.type) return false;
+  switch (a.type) {
+    case VAL_BOOL:   return AS_BOOL(a) == AS_BOOL(b);
+    case VAL_NULL:    return true;
+    case VAL_NUMBER: return AS_NUMBER(a) == AS_NUMBER(b);
+    default:         return false; 
+  }
 }
