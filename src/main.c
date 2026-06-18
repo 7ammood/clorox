e005962e5ff5dc0c9d7be39082a90a7a2fc35f6b
7amood <mohammadsulaiman206@gmail.com>
2026-06-18 20:56:05 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,81 +2,81 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "common.h"
 #include "chunk.h"
+#include "common.h"
 #include "debug.h"
 #include "vm.h"
 
-static void repl(){
-    char line[1024];
-    for(;;){
-        printf(">");
+static void repl() {
+  char line[1024];
+  for (;;) {
+    printf(">");
 
-        if(fgets(line,sizeof(line), stdin) == NULL){
-            printf("\n");
-            break;
-        }
-
-        interpret(line);
+    if (fgets(line, sizeof(line), stdin) == NULL) {
+      printf("\n");
+      break;
     }
+
+    interpret(line);
+  }
 }
 
-static char* readFile(const char* path){
-    FILE* file = fopen(path,"rb");
-    //checking if file is opened
-    if (file == NULL) {
+static char *readFile(const char *path) {
+  FILE *file = fopen(path, "rb");
+  // checking if file is opened
+  if (file == NULL) {
     fprintf(stderr, "Could not open file \"%s\".\n", path);
     exit(74);
-    }
+  }
 
-    fseek(file,0L,SEEK_END);
-    size_t fileSize = ftell(file);
-    rewind(file);   
+  fseek(file, 0L, SEEK_END);
+  size_t fileSize = ftell(file);
+  rewind(file);
 
-    
-    char* buffer = (char*)malloc(fileSize + 1);
-    //checking malloc if it failed
-    if (buffer == NULL) {
+  char *buffer = (char *)malloc(fileSize + 1);
+  // checking malloc if it failed
+  if (buffer == NULL) {
     fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
     exit(74);
-    }
+  }
 
-    size_t bytesRead = fread(buffer,sizeof(char),fileSize,file);
-    //checking if all the file was read into the buffer
-    if (bytesRead < fileSize) {
+  size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
+  // checking if all the file was read into the buffer
+  if (bytesRead < fileSize) {
     fprintf(stderr, "Could not read file \"%s\".\n", path);
     exit(74);
-    }
+  }
 
-    buffer[bytesRead] = '\0';
+  buffer[bytesRead] = '\0';
 
-    fclose(file);
-    return buffer;
+  fclose(file);
+  return buffer;
 }
 
-static void runFile(const char* path){
-    char* source = readFile(path);
-    InterpreterResult result = interpret(source);
-    free(source);
+static void runFile(const char *path) {
+  char *source = readFile(path);
+  InterpreterResult result = interpret(source);
+  free(source);
 
-    if(result == INTERPRET_COMPILE_ERROR) exit(65);
-    if(result == INTERPRET_RUNTIME_ERROR) exit(70);
+  if (result == INTERPRET_COMPILE_ERROR)
+    exit(65);
+  if (result == INTERPRET_RUNTIME_ERROR)
+    exit(70);
 }
 
-int main(int argc, char *argv[]) { 
+int main(int argc, char *argv[]) {
 
-    initVM();
+  initVM();
 
-    if(argc == 1){
-        repl();
-    } else if(argc == 2){
-        //runFile(argv[1]);
-    } else {
-        fprintf(stderr,"Usage: clorox [path]\n");
-        exit(64);
-    }
+  if (argc == 1) {
+    repl();
+  } else if (argc == 2) {
+    // runFile(argv[1]);
+  } else {
+    fprintf(stderr, "Usage: clorox [path]\n");
+    exit(64);
+  }
 
-    freeVM();
-    return 0; 
-
+  freeVM();
+  return 0;
 }
